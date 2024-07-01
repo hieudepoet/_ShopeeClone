65c139c3db3145708dcb669cc0c18c594dff77a9
hieudepoet <dinhviethieu2910@gmail.com>
2024-07-01 11:20:28 +0700

    None

--- src/model/user/Customer.java
+++ src/model/user/Customer.java
@@ -1,60 +1,20 @@
 package model.user;
 
-import java.util.Scanner;
-
 import model.User;
 
 public class Customer extends User{
-    private String phone_number;
+    private boolean isLogged;
 
-    public Customer(int userID, String username, String email, String password, String phone_number) {
+    public Customer(int userID, String username, String email, String password, boolean isLogged) {
         super(userID, username, email, password);
-        this.phone_number = phone_number;
+        this.isLogged = isLogged;
     }
 
-    public String getPhone_number() {
-        return phone_number;
+    public boolean isLogged() {
+        return isLogged;
     }
 
-    public void setPhone_number(String phone_number) {
-        this.phone_number = phone_number;
-    }
-
-    public void login() {
-        
-                
-        String username , password;
-        Scanner sc = new Scanner(System.in);
-
-        System.out.print("Enter username : ");username = sc.nextLine();
-        System.out.print("Enter password : ");password = sc.nextLine();
-
-        for (User user : CustomerManagement.getCustomers().values()){
-
-            if (user.getUsername().equals(username)){
-                if(user.getPassword().equals(password)){
-                    System.out.println();
-                    
-                }
-            }
-
-        }
-        
-
-        sc.close();
-    }
-
-    public void logout() {
-
-        
-    }
-
-    public void register() {
-        if (!CustomerManagement.getCustomers().containsKey(getUsername())) {
-            CustomerManagement.getCustomers().put(getUsername(), this);
-            System.out.println("Registration successful for user: " + getUsername());
-        } else {
-            System.out.println("Username already exists: " + getUsername());
-        }
+    public void setLogged(boolean isLogged) {
+        this.isLogged = isLogged;
     }
 }
--- src/model/user/CustomerManagement.java
+++ src/model/user/CustomerManagement.java
@@ -4,19 +4,57 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class CustomerManagement {
-    private static Map<String, Customer> customers = new HashMap();
-    private boolean loggedIn;
+    private static Map<Integer, Customer> customers;
+    private int nextCustomerID;
 
-    public static Map<String, Customer> getCustomers() {
+    public CustomerManagement() {
+        customers = new HashMap<>();
+        nextCustomerID = 1;
+    }
+
+    public int register(String user_name, String email, String password) {
+        int customerID = nextCustomerID++;
+        Customer customer = new Customer(customerID, user_name, email, password, false);
+        customers.put(customerID, customer);
+        return customerID;
+    }
+
+    public Customer login(String user_name, String password) {
+        for (Customer customer : customers.values()) {
+            if (customer.getUsername().equals(user_name) && customer.getPassword().equals(password)) {
+                customer.setLogged(true);
+                System.out.println("Login successful.");
+                return customer;
+            } 
+        }
+
+        System.out.println("Invalid username or password.");
+        return null;
+    }
+
+    public void logout(Customer customer) {
+        if (customer != null && customer.isLogged()) {
+            customer.setLogged(false);
+            System.out.println("Logout successful.");
+        }
+        else {
+            System.out.println("Error.");
+        }
+    }
+
+    public Customer getCustomer(int customerID) {
+        return customers.get(customerID);
+    }
+
+    public static Map<Integer, Customer> getCustomers() {
         return customers;
     }
-    public static void setCustomers(Map<String, Customer> customers) {
+
+    public static void setCustomers(Map<Integer, Customer> customers) {
         CustomerManagement.customers = customers;
     }
-    public boolean isLoggedIn() {
-        return loggedIn;
-    }
-    public void setLoggedIn(boolean loggedIn) {
-        this.loggedIn = loggedIn;
+
+    public void removeCustomer(int customerID) {
+        customers.remove(customerID);
     }
 }
